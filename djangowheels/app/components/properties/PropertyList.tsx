182785afa67eb498c324f82b8d8b1d6849eb73af
Ayim William <odhisayim@gmail.com>
2024-11-25 15:03:11 +0300

    added PropertyList and PropertyListItem

---
+++ djangowheels/app/components/properties/PropertyList.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropertyListItem from './PropertyListItem';
+
+const PropertyList = () => {
+  return (
+    <div className="">
+        <PropertyListItem />
+        <PropertyListItem />
+        <PropertyListItem />
+
+    </div>
+    
+  )
+}
+
+export default PropertyList;
