38cdb98ae4c1f4591185abaf07229a3006f87df8
Ayim William <odhisayim@gmail.com>
2024-12-07 18:37:44 +0300

    user menu pop up partially coded

--- nomadWheels_App/app/components/navbar/UserNav.tsx
+++ nomadWheels_App/app/components/navbar/UserNav.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+'use client';
+import React from 'react';
+import {useState} from 'react';
+import MenuLink from './MenuLink';
 
 const UserNav = () => {
+  const[isOpen, setIsOpen] = useState(true)
   return (
     <div className='p-2 relative inline-block border rounded-full'>
         <button className="flex items center">
@@ -12,6 +16,19 @@ const UserNav = () => {
             </svg>
 
         </button>
+
+        {isOpen && (
+          <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
+            <MenuLink 
+                label="Log in"
+                onClick={() => console.log("clicked button")}
+            />
+            <MenuLink 
+                label="Sign up"
+                onClick={() => console.log("clicked button")}
+            />
+          </div>
+        )}
     </div>
   )
 }
