53389eb26a3e7d1089150e6232f2a23c06fe42af
Ayim William <odhisayim@gmail.com>
2024-12-12 10:41:43 +0300

    created new modal

---
+++ nomadWheels_App/app/components/modals/Modal.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const Modal: React.FC = () => {
+    return (
+        <div className="">
+            asdf
+        </div>
+    )
+}
+
+export default Modal; 
