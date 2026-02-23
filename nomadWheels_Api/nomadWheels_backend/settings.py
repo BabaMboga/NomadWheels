cdf4eb70f41bb4f5d9f2097f4a42da0a3adf0210
Ayim William <odhisayim@gmail.com>
2026-02-23 16:34:20 +0300

    changed docker-compose.yml

--- nomadWheels_Api/nomadWheels_backend/settings.py
+++ nomadWheels_Api/nomadWheels_backend/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c7nk)n$v0nrqbl6qkd@c5t-zfdfg#+s%rft5m-8r)12*ogd)c!'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="").split(" ")
 
 
 # Application definition
