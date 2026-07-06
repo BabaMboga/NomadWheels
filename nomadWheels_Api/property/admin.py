0d313e6398a86556a8f9e8eb6d31fa6c46df6185
Ayim William <odhisayim@gmail.com>
2026-07-06 20:58:19 +0300

    feat(Property): Changed admin.py to ensure django admin can add image

--- nomadWheels_Api/property/admin.py
+++ nomadWheels_Api/property/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Property
+from .models import Property, PropertyImage
 
-admin.site.register(Property)
+# Replaced the below because it doesnt allow for admin to show image attribute directly
+# admin.site.register(Property)
+
+class PropertyImageInline(admin.TabularInline):
+    model = PropertyImage
+    extra = 2
+    fields = ['image', 'is_primary', 'caption']
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'make', 'model', 'year', 'price_per_day', 'is_available', 'created_at']
+    inlines = [PropertyImageInline]
+    fields = [
+        'title', 'description', 'price_per_day',
+        'make', 'model', 'year', 'license_plate',
+        'color', 'seats', 'transmission', 'fuel_type',
+        'country', 'country_code', 'city', 'address', 'category',
+        'favorited', 'owner', 'is_available', 'is_insured',
+    ]
+
+@admin.register(PropertyImage)
+class PropertyImageAdmin(admin.ModelAdmin):
+    list_display = ['property_listing', 'caption', 'is_primary', 'created_at']
+    list_filter = ['is_primary', 'created_at']
