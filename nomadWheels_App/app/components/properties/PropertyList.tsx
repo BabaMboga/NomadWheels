a582ef64530b88c10112cf29332af8a73079cd03
Ayim William <odhisayim@gmail.com>
2026-07-07 17:17:11 +0300

    feat(properties): fetches from api/properties instead of static

--- nomadWheels_App/app/components/properties/PropertyList.tsx
+++ nomadWheels_App/app/components/properties/PropertyList.tsx
@@ -1,7 +1,30 @@
+'use client';
+
+
 import React from 'react';
+import { useEffect, useState } from 'react';
 import PropertyListItem from './PropertyListItem';
 
 const PropertyList = () => {
+
+  const getProperties = async () => {
+    const url = 'http://localhost:8080/api/properties/';
+
+    await fetch(url, {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then((json) => {
+        console.log('json', json);
+      })
+      .catch((error) => {
+        console.log('error',error)
+      });
+  };
+
+  useEffect(() => {
+    getProperties();
+  }, []);
   return (
     <>
         <PropertyListItem />
