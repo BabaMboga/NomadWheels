63c245bfd0434c662bbe621a540d4eadecff3d52
Ayim William <odhisayim@gmail.com>
2026-07-08 21:38:23 +0300

    feat(property): conditional rendering to handle noimage

--- nomadWheels_App/app/components/properties/PropertyListItem.tsx
+++ nomadWheels_App/app/components/properties/PropertyListItem.tsx
@@ -10,16 +10,24 @@ interface PropertyProps {
 const PropertyListItem: React.FC<PropertyProps> = ({
     property
 }) => {
+    const imageUrl = property.primary_image_url || '/nomadWheels_App/public/properties/car-holder.jpg';
   return (
     <div className="cursor-pointer">
         <div className="relative overflow-hidden aspect-square rounded-xl">
-            <Image 
+            {property.primary_image_url ? (
+                <Image 
                 fill
-                src = {property.primary_image_url}
+                src = {imageUrl}
                 alt = "property image"
                 sizes = "(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
                 className='hover:scale-110 object-cover transition h-full w-full'
             />
+            ):(
+                <div className='flex items-center justify-center h-full w-full text-gray-500'>
+                    No Image
+                </div>
+            )}
+            
         </div>
         
         <div className="mt-2">
